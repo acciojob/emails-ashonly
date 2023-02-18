41de9d4597762bc94b31d4d10d79891c2a93eb9b
ash <ash@ashs-iMac.local>
2023-02-18 23:16:15 +0530

    Email contest First Attempt

---
+++ src/main/java/com/driver/Emailcontainer.java
@@ -0,0 +1,14 @@
+package com.driver;
+
+import java.util.Date;
+
+public class Emailcontainer {
+    Date date;
+    String sender;
+    String message;
+    public Emailcontainer(Date date, String sender, String message) {
+        this.date = date;
+        this.sender = sender;
+        this.message = message;
+    }
+}
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -1,5 +1,6 @@
 package com.driver;
 
+import com.google.common.primitives.UnsignedInteger;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.time.LocalTime;
@@ -12,12 +13,13 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+    super(emailId, Integer.MAX_VALUE);
+    this.calendar=new ArrayList<>();
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
-
+     calendar.add(meeting);
     }
 
     public int findMaxMeetings(){
@@ -25,6 +27,22 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-
+     ArrayList<Pair<LocalTime,Integer>> endtimes=new ArrayList<>();
+     for(int i=0;i<calendar.size();i++){
+         endtimes.add(Pair.of(calendar.get(i).getEndTime(),i));
+     }
+     Collections.sort(endtimes);
+     LocalTime Timelimit=endtimes.get(0).getLeft();
+     int count=0;
+     if(!endtimes.isEmpty()){
+         count+=1;
+     }
+     for(int i=1;i<endtimes.size();i++){
+         if(calendar.get(endtimes.get(i).getRight()).getStartTime().compareTo(Timelimit)>0){
+             count=count+1;
+             Timelimit=endtimes.get(i).getLeft();
+         }
+     }
+     return count;
     }
 }
